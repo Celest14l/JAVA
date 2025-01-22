bf8760d7e8e6c17025c39c9bf765b84b584b1e54
Priyanshu Singh <142790017+Celest14l@users.noreply.github.com>
2025-01-23 01:41:12 +0530

    Add files via upload

---
+++ Array Functions/Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String args[]) {
+        UserInput in = new UserInput();
+        ArrayFunctions functions = new ArrayFunctions();
+
+        functions.oddEven(in.arrayInput());
+
+    }
+}
